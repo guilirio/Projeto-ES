bedea629e9c01fde50f0a2e58d5cbfcd102cf776
Fabio Valim <oficialvalim@gmail.com>
2025-11-14 10:27:57 -0300

    feat: adicionando rotas do CRUD de usuarios

---
+++ app/backend/src/routes/UsuarioRoutes.js
@@ -0,0 +1,12 @@
+const { Router } = require('express');
+const UsuarioController = require('../controllers/UsuarioController');
+
+const router = Router();
+
+router.post('/usuarios', UsuarioController.create);
+router.get('/usuarios', UsuarioController.getAll);
+router.get('/usuarios/:id', UsuarioController.getById);
+router.put('/usuarios/:id', UsuarioController.update);
+router.delete('/usuarios/:id', UsuarioController.delete);
+
+module.exports = router;
