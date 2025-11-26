bed4a73f9f59eb6e0ff0056c035822df3cff9a2b
Fabio Valim <oficialvalim@gmail.com>
2025-11-26 08:51:11 -0300

    feat: adiciona rota para login do usuario

--- app/backend/src/routes/index.js
+++ app/backend/src/routes/index.js
@@ -1,5 +1,6 @@
 const { Router } = require('express');
 const usuarioRoutes = require('./UsuarioRoutes');
+const authRoutes = require('./AuthRoutes');
 
 /**
  * Arquivo responsável por agrupar todas as rotas
@@ -8,6 +9,7 @@ const usuarioRoutes = require('./UsuarioRoutes');
 const router = Router();
 
 router.use(usuarioRoutes);
+router.use(authRoutes);
 // router.use(veiculoRoutes);
 // router.use(categoriaRoutes);
 // router.use(locacaoRoutes);
